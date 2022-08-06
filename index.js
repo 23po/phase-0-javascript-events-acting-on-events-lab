5fbc580e59122cc94dd025c701b82b2e45922d19
codalo <ckodalo@gmail.com>
2022-08-07 02:22:30 +0300

    updated with best solution

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ function moveDodgerLeft() {
   }
 
   function moveDodgerRight() {
+    
     const fake = document.getElementById("fake")
     const game = document.getElementById("game");
-    const game_max_width = game.offsetWidth
+    const game_max_width = game.getBoundingClientRect();
     const int_game_max_width = parseInt(game_max_width, 10); 
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
+    
 
-    if ( left !== int_game_max_width) {
+    if ( left !== 360) {
         dodger.style.left = `${left + 1}px`;
     }
   }
